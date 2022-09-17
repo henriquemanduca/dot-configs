32d4e424e8ff937c87be3f69aca9f901679449c7
Henrique <henrique.manduca@softfocus.com.br>
2022-09-17 11:09:57 -0300

    Update

--- config.fish
+++ config.fish
@@ -2,33 +2,38 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-oh-my-posh init fish --config ~/.poshthemes/blue-owl.omp.json | source
+# Bins  -------------------------------
+
+# Detekt
+fish_add_path /home/henrique/Programas/detekt-cli-1.21.0/bin/
+
+oh-my-posh --init --shell fish --config ~/.poshthemes/blue-owl.omp.json | source
 
 alias src='source ~/.config/fish/config.fish'
+alias edit-fish='code ~/.config/fish/config.fish'
 
 # Apps --------------------------------------
 alias g='git'
-alias nt='nautilus'
-alias el='exa --long --header --icons'
+alias detekt-html='detekt-cli -r html:/home/henrique/detekt-reports/detekt.html'
 
 # Commands ----------------------------------
 alias cl='clear'
 
-#alias rm='rm -i'            	# confirm removal
-alias cp='cp -i'            	# confirm copy
-alias mv='mv -i'            	# confirm move
-alias rmf='rm -rf'          	# just delete it
+alias rm='rm -i'          # confirm removal
+alias cp='cp -i'          # confirm copy
+alias mv='mv -i'          # confirm move
+alias rmf='rm -rf'        # just delete it
 
-alias fsize='sudo du -sh'   	# folder size
+alias fsize='sudo du -sh' # folder size
 
 alias ht='history'
 alias htg='history | grep'
 
 alias fpid='ps aux | grep -i'
-alias kid='sudo kill -9' 	# -9: SIGKILL
+alias kid='sudo kill -9'  # -9: SIGKILL
 
 # Configs -----------------------------------
-alias hts='sudo nano /etc/hosts'
+alias hts='kate /etc/hosts'
 
 # Apt ---------------------------------------
 alias up='sudo apt update'
@@ -44,25 +49,72 @@ alias instd='sudo dpkg -i'
 alias ..='cd ..'
 alias ...='cd ../..'
 
-alias prj='cd /home/henrique/Projetos/'
+function home
+    cd ~
+end
+
+function projs
+    cd ~/Projetos/
+end
+
+function pess
+    projs
+    cd pessoal/
+end
+
+function soft
+    projs
+    cd softfocus/
+end
+
+function bkpd
+    sudo rsync -aAXv . --dry-run --delete --exclude={/dev/*,/lost+found}  /backup
+end
+
+function bkp
+    sudo rsync -aAXv . --delete --exclude={/dev/*,/lost+found}  /backup
+end
+
+# Repos -------------------------------------
+
+function update-fish-repo
+    rsync -av ~/.config/fish/config.fish ~/Projetos/pessoal/dot-configs/ 
+    git -C ~/Projetos/pessoal/dot-configs add config.fish
+    git -C ~/Projetos/pessoal/dot-configs commit -m "Update"
+end
+
+function update-code-repo
+    rsync -av ~/.config/fish/config.fish ~/Projetos/pessoal/dot-configs/
+end
 
 # DOCKER ------------------------------------
 alias dk='docker'
 alias dks='docker start'
 alias dkt='docker stop'
 
-# Minikube ----------------------------------
+function ptst
+   soft
+   cd databases-on-docker/
+   docker compose -f postgres12postgis3.yml up -d
+   home
+end
+
+function ptsp
+    soft
+    cd databases-on-docker/
+    docker compose -f postgres12postgis3.yml down
+    home
+end
+
+# Kubernets ----------------------------------
 alias skaf='skaffold dev --port-forward'
 alias skafb='skaffold build'
 
-alias kbs='minikube start'
-alias kbt='minikube stop'
-
-alias kbip='minikube ip'
 alias kbip2='kubectl get svc'
 
 # Node ----------------------------------
 alias nrs='npm run start'
 alias rmnm='rm -Rf node_modules/ && rm package-lock.json'
+
 alias ni='npm install'
 alias nif='npm install --force'

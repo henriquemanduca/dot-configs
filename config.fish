c48c16d1f2cf9d843b2428f18b7401c09cf0400c
Henrique <henrique.manduca@softfocus.com.br>
2022-11-18 12:01:25 -0300

    Update

--- config.fish
+++ config.fish
@@ -10,10 +10,11 @@ fish_add_path /home/henrique/Programas/detekt-cli-1.21.0/bin/
 oh-my-posh --init --shell fish --config ~/.poshthemes/blue-owl.omp.json | source
 
 # Configs -----------------------------
-alias src='source ~/.config/fish/config.fish'
-alias edit-fish='nvim ~/.config/fish/config.fish'
-alias edit-hosts='sudo nvim /etc/hosts'
-alias edit-nvim='nvim ~/.config/nvim/init.vim'
+alias src='source ~/.config/fish/config.fish && cl'
+alias ed-fish='nvim ~/.config/fish/config.fish'
+alias ed-fish-code='code ~/.config/fish/config.fish'
+alias ed-hosts='sudo nvim /etc/hosts'
+alias ed-nvim='nvim ~/.config/nvim/init.vim'
 
 # Apps --------------------------------
 
@@ -32,18 +33,19 @@ alias lr='ls -R'
 alias lh='ls -d .*'
 
 alias cl='clear'
+alias p='pwd'
 
 alias rm='rm -i'          # confirm removal
 alias cp='cp -i'          # confirm copy
 alias mv='mv -i'          # confirm move
 alias rmf='rm -rf'        # just delete it
 
-alias fsize='sudo du -sh' # folder size
+alias fd-size='sudo du -sh' # folder size
 
 alias ht='history'
-alias htg='history | grep'
+alias htf='history | grep'
 
-alias fpid='ps aux | grep -i'
+alias idf='ps aux | grep -i'
 alias kid='sudo kill -9'  # -9: SIGKILL
 
 # Apt ---------------------------------
@@ -56,7 +58,7 @@ alias upd='sudo apt dist-upgrade'
 
 alias upclean='sudo apt clean && sudo apt autoclean && sudo apt autoremove --purge'
 
-alias insta='sudo apt install'
+alias inst='sudo apt install'
 alias instd='sudo dpkg -i'
 
 # Folders -----------------------------
@@ -69,17 +71,17 @@ function home
     clear
 end
 
-function projs
+function Projetos
     cd ~/Projetos/
 end
 
-function pess
-    projs
+function pessoal
+    Projetos
     cd pessoal/
 end
 
-function soft
-    projs
+function softfocus2
+    Projetos
     cd softfocus2/
 end
 
@@ -93,14 +95,14 @@ end
 
 # Repos -----------------------------
 
-function update-fish-repo
+function up-fish-repo
     rsync -av ~/.config/fish/config.fish ~/Projetos/pessoal/dot-configs/ 
     git -C ~/Projetos/pessoal/dot-configs add config.fish
     git -C ~/Projetos/pessoal/dot-configs commit -m "Update"
     git -C ~/Projetos/pessoal/dot-configs push
 end
 
-function update-keepass-repo
+function up-keepass-repo
     git -C /home/henrique/Projetos/pessoal/keepass add manduca.kdbx
     git -C /home/henrique/Projetos/pessoal/keepass commit -m "Update"
     git -C /home/henrique/Projetos/pessoal/keepass push
@@ -128,20 +130,90 @@ end
 
 # Kubernets -----------------------------
 
-alias skaf='clear && skaffold dev --port-forward'
-alias skafb='clear && skaffold build'
+alias skf='clear && skaffold dev --port-forward'
+alias skf-b='clear && skaffold build'
 
-alias ksvc='kubectl get svc'
+alias kube-svc='kubectl get svc'
+
+# Local
 
 alias lpods='kubectl get pods'
 alias llogs='kubectl logs '
 alias ldesc='kubectl describe pod '
-alias lrest='kubectl rollout restart deployment '
+alias lroll='kubectl rollout restart deployment '
 
-alias hpods='kubectl --kubeconfig kubectl_config_hml.conf get pods'
-alias hlogs='kubectl --kubeconfig kubectl_config_hml.conf logs'
-alias hdesc='kubectl --kubeconfig kubectl_config_hml.conf describe pod'
-alias hrest='kubectl --kubeconfig kubectl_config_hml.conf rollout restart deployment '
+alias kubepath='/usr/local/bin/kubectl --kubeconfig ~/Projetos/softfocus2/kubectl/kubectl_config_hml.conf'
+
+set nameSpaceInterno 'credisol-interno'
+
+# Interno
+
+function ipods 
+    kubepath get pods -n $nameSpaceInterno
+end
+
+function ipodsf
+    ipods | grep $argv
+end
+
+function ilogs 
+    kubepath logs $argv -n $nameSpaceInterno
+end
+
+function ilogsf
+    ilogs (string split ' ' (ipodsf $argv))[1]    
+end
+
+function idesc 
+    kubepath describe pod $argv -n $nameSpaceInterno
+end
+
+function idesf
+    idesc (string split ' ' (ipodsf $argv))[1]    
+end
+
+function iroll 
+    kubepath rollout restart deployment $argv -n $nameSpaceInterno
+end
+
+function irollf
+    iroll (string split ' ' (ipodsf $argv))[1]    
+end
+
+# Homolog
+
+function hpods 
+    kubepath get pods
+end
+
+function hpodsf
+    hpods | grep $argv
+end
+
+function hlogs 
+    kubepath logs $argv
+end
+
+function hlogsf
+    hlogs (string split ' ' (hpodsf $argv))[1]    
+end
+
+function hdesc 
+    kubepath describe pod $argv
+end
+
+function hdesf
+    hdesc (string split ' ' (hpodsf $argv))[1]    
+end
+
+function hroll 
+    kubepath rollout restart deployment $argv
+end
+
+function hrollf
+    hroll (string split ' ' (hpodsf $argv))[1]    
+end
+
 
 # Node ----------------------------------
 
